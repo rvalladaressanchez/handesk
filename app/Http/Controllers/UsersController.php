9917e94cadfe9c1adbec3a2e496db7bd36fd7739
Rodrigo Valladares <ineolitei@gmail.com>
2019-02-15 16:16:50 -0300

    Nuevo usuario, eliminar usuario e impersonar usuario quedan registradas

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,14 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use App\User;
+use App\Registro;
 use App\Language;
+use DB;
 
 class UsersController extends Controller
 {
     public function index()
     {
         $users = User::with('teams')->paginate(25);
-
         return view('users.index', ['users' => $users]);
     }
     public function create(){
@@ -19,8 +20,8 @@ class UsersController extends Controller
     }
     public function store(){
         //$this->authorize('create', User::class);
-        $admin = 0;
-        if(request('chkAdmin')=='on') $admin = 1;
+        if(request('chkAdmin')!=null) $admin = 1;
+        else $admin = 1;
         $usuarioCreado=User::create([
             'name'              => request('name'),
             'email'             => request('email'),
@@ -28,29 +29,21 @@ class UsersController extends Controller
             'password'          => bcrypt(request('pass')),
             'admin'             => $admin,
         ]);
-        $comentario = 'El usuario ('.Auth::user()->id.') '.Auth::user()->name.' ha creado al usuario ('.$usuarioCreado->id.')'.$usuarioCreado->name.'.';
-        $this->registrar(Auth::user()->id,'Usuario creado',$comentario);
+        Registro::registrar(Auth::user()->id,'usuario.creado', 'El usuario ('.Auth::user()->id.') '.Auth::user()->name.' ha creado al usuario ('.$usuarioCreado->id.')'.$usuarioCreado->name.'.');
         return redirect()->route('users.index');
-    }
-    private function registrar($usuario,$accion,$comentario){
-        DB::table('registros')->insert([
-            'usuario' => $usuario,
-            'accion' => $accion,
-            'comentario' =>$comentario
-        ]);
-        return true;
     }
     public function delete(User $user)
     {
         if(Auth::user()->id==$user->id) return back();
         $user->delete();
+        Registro::registrar(Auth::user()->id,'usuario.eliminado', 'El usuario ('.Auth::user()->id.') '.Auth::user()->name.' ha eliminado al usuario ('.$user->id.')'.$user->name.'.');
         return back();
     }
 
     public function impersonate(User $user)
     {
         auth()->loginUsingId($user->id);
-
+        Registro::registrar(Auth::user()->id,'impersonar','El usuario ('.Auth::user()->id.')'.Auth::user()->name.' ha impersonado al usuario ('.$user->id.') '.$user->name.'.');
         return redirect()->route('tickets.index');
     }
 }
