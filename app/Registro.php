3610c4854102376f52537c6895ca337fc2b3b62e
Rodrigo Valladares <ineolitei@gmail.com>
2019-02-15 16:15:01 -0300

    Se crea clase Registro y funcion para registrar cambios

--- app/Registro.php
+++ app/Registro.php
@@ -4,5 +4,14 @@ namespace App;
 
 class Registro extends BaseModel
 {
-    
+    public static function registrar($usuario, $accion, $comentario){
+        DB::table('registros')->insert([
+            'usuario' => $usuario,
+            'accion' => $accion,
+            'comentario' =>$comentario,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+        return true;
+    }
 }
