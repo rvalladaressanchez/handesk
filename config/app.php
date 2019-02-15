b385a291e339b809f459cbd1e132f010d8632950
Rodrigo Valladares <ineolitei@gmail.com>
2019-02-15 15:44:07 -0300

    Eliminado ServiceProvider innecesario

--- config/app.php
+++ config/app.php
@@ -164,7 +164,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        App\Providers\RegistrosServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -231,7 +230,7 @@ return [
         'URL'          => Illuminate\Support\Facades\URL::class,
         'Validator'    => Illuminate\Support\Facades\Validator::class,
         'View'         => Illuminate\Support\Facades\View::class,
-        'Registro'     => App\Facades\Registro::class,
+        'Registro'     => App\Registro::class,
 
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
